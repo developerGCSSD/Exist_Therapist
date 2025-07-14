1add708bb2e9abc92261ba4cf8f553e115dea277
Fady Nashat <fidoo.nashat@gmail.com>
2025-07-14 13:37:53 +0300

    make the app in js

---
+++ App.js
@@ -0,0 +1,33 @@
+// App.js
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.text}>Welcome to React Native JS !</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
+
+export default App;
